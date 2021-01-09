a512658167a88735455df4f3555adce6bbf93e4d
Carlos Hidalgo <carloshidalgolache@gmail.com>
2021-01-09 00:56:23 -0500

    Subiendo el proyecto API REST en VB! :)

---
+++ Engine/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Engine.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Engine/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Engine.WebApiApplication" Language="C#" %>
